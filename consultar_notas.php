2e630d09785637a382aec02c460d5c6cd7e91a8c
Welder Azevedo <Welder Azevedo>
2016-11-04 20:35:21 -0200

    Parte de consulta de notas pelo aluno e pelo professor, falta pela instituicao

--- consultar_notas.php
+++ consultar_notas.php
@@ -49,12 +49,37 @@ $resultado = $conexao->executaComando("SELECT * FROM notas WHERE user_email = '$
 					<center>
 					<div id="buscar_notas" class="btn-group" data-toggle="buttons">
 						<div id="div_busca_notas" class="input-group">
-							<input type="text" id="txt_busca" class="form-control"/>
+							<input type="text" id="txt_busca" name="txt_busca" class="form-control"/>
 							<select id="slc_opcaoBusca" name="slc_opcaoBusca" class="form-control">
-								<option>Questionário</option>
-								<option>Matéria</option>
-								<option>Aluno</option>
-								<option>Professor</option>
+								
+								<?php
+
+									if($_SESSION['user_tipo'] == 'I'){
+										?>
+											<option value='questionarios.quest_nome'>Questionário</option>
+											<option value='materias.materia_nome'>Matéria</option>
+											<option value='concat (usuarios.user_nome, ' ', alunos.aluno_sobrenome)'>Aluno</option>
+											<option value='professores.professor_email'>Professor</option>
+										<?php	
+									}
+
+									if($_SESSION['user_tipo'] == 'P'){
+										?>
+											<option value='questionarios.quest_nome'>Questionário</option>
+											<option value='materias.materia_nome'>Matéria</option>
+											<option value="concat (usuarios.user_nome, ' ', alunos.aluno_sobrenome)">Aluno</option>
+										<?php
+									}
+
+									if($_SESSION['user_tipo'] == 'A'){
+										?>
+											<option value='questionarios.quest_nome'>Questionário</option>
+											<option value='materias.materia_nome'>Matéria</option>
+										<?php
+									}
+
+								?>
+							
 							</select>
 							<span class="input-group-btn">
 								<button id="btn_buscar" class="btn btn-primary">Buscar</button>
@@ -69,11 +94,29 @@ $resultado = $conexao->executaComando("SELECT * FROM notas WHERE user_email = '$
 				<div class="col-md-12">
 					<table id="tabela_notas_alunos" class="table tablesorter">
 						<thead>
-							<th>Questionário</th>
-							<th>Professor</th>
-							<th>Matéria</th>
-							<th>Data que foi respondido</th>
-							<th>Nota</th>
+							<?php
+								if($_SESSION['user_tipo'] == 'A'){
+									echo "<th>Questionário</th>
+									<th>Professor</th>
+									<th>Matéria</th>
+									<th>Data que foi respondido</th>
+									<th>Nota</th>";
+								}
+								if($_SESSION['user_tipo'] == 'P'){
+									echo "<th>Aluno</th>
+									<th>Questionário</th>
+									<th>Matéria</th>
+									<th>Data que foi respondido</th>
+									<th>Nota</th>";
+								}
+								if($_SESSION['user_tipo'] == 'I'){
+									echo "<th>Questionário</th>
+									<th>Professor</th>
+									<th>Matéria</th>
+									<th>Data que foi respondido</th>
+									<th>Nota</th>";
+								}
+							?>
 						</thead>
 						<tbody id="corpo_tabela">
 							<tr id='tr_sem_notas'>
@@ -99,11 +142,25 @@ $resultado = $conexao->executaComando("SELECT * FROM notas WHERE user_email = '$
 				notas_alunos();
 			});
 
+			$('#txt_busca').keypress(function(e){
+				 if(e.which == 13) {
+					notas_alunos();
+				 }
+			});
+
+			$('#btn_buscar').click(function(){
+				notas_alunos();
+			});
+
 			function notas_alunos(){
+				var usuario_tipo = '<?php echo $_SESSION['user_tipo']; ?>';
+				var select = $('#slc_opcaoBusca').val();
+				var busca = $('#txt_busca').val();
 				var linhas = "";
 				$.ajax({
 					type: "POST",
 					url: "scripts/notas_alunos.php",
+					data: "slc_opcaoBusca="+select+"&txt_busca="+busca,
 					dataType: "json",
 					success: function(retorno){
 						if(retorno == 0){
@@ -111,13 +168,33 @@ $resultado = $conexao->executaComando("SELECT * FROM notas WHERE user_email = '$
 						}else{
 							var contador = 1;
 							retorno.forEach(function(item){
-								linhas += "<tr>";
-									linhas += "<td>"+item['quest_nome']+"</td>";
-									linhas += "<td>"+item['quest_professor']+"</td>";
-									linhas += "<td>"+item['quest_materia']+"</td>";
-									linhas += "<td>"+item['data_resposta']+"</td>";
-									linhas += "<td>"+item['nota_valor']+"</td>";
-								linhas += "</tr>";
+								if(usuario_tipo == 'A'){
+									linhas += "<tr>";
+										linhas += "<td>"+item['quest_nome']+"</td>";
+										linhas += "<td>"+item['quest_professor']+"</td>";
+										linhas += "<td>"+item['quest_materia']+"</td>";
+										linhas += "<td>"+item['data_resposta']+"</td>";
+										linhas += "<td>"+item['nota_valor']+"</td>";
+									linhas += "</tr>";
+								}
+								if(usuario_tipo == 'P'){
+									linhas += "<tr>";
+										linhas += "<td>"+item['quest_aluno']+" "+item['aluno_sobrenome']+"</td>";
+										linhas += "<td>"+item['quest_nome']+"</td>";
+										linhas += "<td>"+item['quest_materia']+"</td>";
+										linhas += "<td>"+item['data_resposta']+"</td>";
+										linhas += "<td>"+item['nota_valor']+"</td>";
+									linhas += "</tr>";
+								}
+								if(usuario_tipo == 'I'){
+									linhas += "<tr>";
+										linhas += "<td>"+item['quest_nome']+"</td>";
+										linhas += "<td>"+item['quest_professor']+"</td>";
+										linhas += "<td>"+item['quest_materia']+"</td>";
+										linhas += "<td>"+item['data_resposta']+"</td>";
+										linhas += "<td>"+item['nota_valor']+"</td>";
+									linhas += "</tr>";
+								}
 							});
 						
 						}
--- scripts/notas_alunos.php
+++ scripts/notas_alunos.php
@@ -9,33 +9,102 @@
     date_default_timezone_set("America/Sao_Paulo");
     $dataAtual = date('Y-m-d H:i:s');
 
+	$tipo_busca = $_POST['slc_opcaoBusca'];
+	$txt_busca = $_POST['txt_busca'];
+
 	$conexao = new Conexao();
 	$alunos = array();
+
 	$contador = 0; //Serve para contar execução do while
-	$query = "SELECT distinct questionario_aluno.user_email, questionario_aluno.data_resposta, questionarios.quest_nome, usuarios.user_nome, professores.professor_sobrenome, notas.nota_valor, notas.`data`, materias.materia_nome FROM questionario_aluno INNER JOIN questionarios ON questionario_aluno.quest_codigo=questionarios.quest_codigo INNER JOIN materias ON materias.materia_codigo=questionarios.quest_materia INNER JOIN usuarios ON usuarios.user_email=questionarios.quest_professor INNER JOIN professores ON professores.professor_email=questionarios.quest_professor INNER JOIN turmas ON turmas.turma_codigo=questionario_aluno.turma_codigo INNER JOIN notas ON notas.quest_codigo=questionario_aluno.quest_codigo WHERE questionario_aluno.user_email = '$_SESSION[user_email]';";
 
-	$resultado = $conexao->executaComando($query);
+	if($_SESSION['user_tipo'] == 'A'){
+		$query = "SELECT distinct questionario_aluno.user_email, questionario_aluno.data_resposta, questionarios.quest_nome, usuarios.user_nome, professores.professor_sobrenome, notas.nota_valor, notas.`data`, materias.materia_nome FROM questionario_aluno INNER JOIN questionarios ON questionario_aluno.quest_codigo=questionarios.quest_codigo INNER JOIN materias ON materias.materia_codigo=questionarios.quest_materia INNER JOIN usuarios ON usuarios.user_email=questionarios.quest_professor INNER JOIN professores ON professores.professor_email=questionarios.quest_professor INNER JOIN turmas ON turmas.turma_codigo=questionario_aluno.turma_codigo INNER JOIN notas ON notas.quest_codigo=questionario_aluno.quest_codigo WHERE $tipo_busca LIKE '%$txt_busca%';";
 
-	if(mysqli_num_rows($resultado) != 0){
-		while($linha = mysqli_fetch_array($resultado)){
+		$resultado = $conexao->executaComando($query);
 
-			$data_resposta_completa = date_create($linha["data_resposta"]);
-			$hora_resposta = date_format($data_resposta_completa, "H:i");
-			$data_resposta = date_format($data_resposta_completa, "d/m/Y");
-			$data_resposta_completa = date_format($data_resposta_completa, "Y-m-d H:i:s");
+		if(mysqli_num_rows($resultado) != 0){
+			while($linha = mysqli_fetch_array($resultado)){
 
-			$questionarios[$contador] = array(
-				"quest_nome" => $linha["quest_nome"],
-				"quest_professor" => $linha["user_nome"],
-				"quest_materia" => $linha["materia_nome"],
-				"data_resposta" => $data_resposta." ".$hora_resposta,
-				"nota_valor" => $linha['nota_valor'],
-			);
+				$data_resposta_completa = date_create($linha["data_resposta"]);
+				$hora_resposta = date_format($data_resposta_completa, "H:i");
+				$data_resposta = date_format($data_resposta_completa, "d/m/Y");
+				$data_resposta_completa = date_format($data_resposta_completa, "Y-m-d H:i:s");
 
-			$contador++;
+				$questionarios[$contador] = array(
+					"quest_nome" => $linha["quest_nome"],
+					"quest_professor" => $linha["user_nome"],
+					"quest_materia" => $linha["materia_nome"],
+					"data_resposta" => $data_resposta." ".$hora_resposta,
+					"nota_valor" => $linha['nota_valor'],
+				);
+
+				$contador++;
+			}
+			echo json_encode($questionarios);
+		}else{
+			echo json_encode(0);
 		}
-		echo json_encode($questionarios);
-	}else{
-		echo json_encode(0);
-	}	
+	}
+	if($_SESSION['user_tipo'] == 'P'){
+		$query = ("SELECT distinct concat (usuarios.user_nome, ' ', alunos.aluno_sobrenome) as nome_aluno, usuarios.user_nome, alunos.aluno_sobrenome, questionarios.quest_nome, questionario_aluno.data_resposta, notas.nota_valor, turmas.turma_nome, materias.materia_nome FROM questionario_aluno INNER JOIN usuarios ON usuarios.user_email=questionario_aluno.user_email INNER JOIN alunos ON alunos.aluno_email=usuarios.user_email INNER JOIN questionarios ON questionarios.quest_codigo=questionario_aluno.quest_codigo INNER JOIN professores ON professores.professor_email=questionarios.quest_professor INNER JOIN notas ON notas.quest_codigo=questionario_aluno.quest_codigo INNER JOIN turmas ON turmas.turma_codigo=questionario_aluno.turma_codigo INNER JOIN materias ON materias.materia_codigo=questionarios.quest_materia	WHERE $tipo_busca like '%$txt_busca%';");
+		$resultado = $conexao->executaComando($query);
+
+		if(mysqli_num_rows($resultado) != 0){
+			while($linha = mysqli_fetch_array($resultado)){
+
+				$data_resposta_completa = date_create($linha["data_resposta"]);
+				$hora_resposta = date_format($data_resposta_completa, "H:i");
+				$data_resposta = date_format($data_resposta_completa, "d/m/Y");
+				$data_resposta_completa = date_format($data_resposta_completa, "Y-m-d H:i:s");
+
+				$questionarios[$contador] = array(
+					"quest_aluno" => $linha["user_nome"],
+					"aluno_sobrenome" => $linha['aluno_sobrenome'],
+					"quest_nome" => $linha["quest_nome"],
+					"quest_materia" => $linha["materia_nome"],
+					"turma_nome" => $linha["turma_nome"],
+					"data_resposta" => $data_resposta." ".$hora_resposta,
+					"nota_valor" => $linha["nota_valor"],
+				);
+
+				$contador++;
+			}
+			echo json_encode($questionarios);
+		}else{
+			echo json_encode(0);
+		}
+	}
+
+						//------------------------------------------CORRIGIR ABAIXO
+	if($_SESSION['user_tipo'] == 'I'){
+		$query = ("SELECT distinct usuarios.user_nome, alunos.aluno_sobrenome, questionarios.quest_nome, notas.`data`, notas.nota_valor, 
+		turmas.turma_nome, materias.materia_nome FROM questionario_aluno INNER JOIN usuarios ON usuarios.user_email=questionario_aluno.user_email INNER JOIN alunos ON alunos.aluno_email=usuarios.user_email INNER JOIN questionarios ON questionarios.quest_codigo=questionario_aluno.quest_codigo INNER JOIN professores ON professores.professor_email=questionarios.quest_professor INNER JOIN notas ON notas.quest_codigo=questionario_aluno.quest_codigo INNER JOIN turmas ON turmas.turma_codigo=questionario_aluno.turma_codigo INNER JOIN materias ON materias.materia_codigo=questionarios.quest_materia WHERE professores.professor_email = '$_SESSION[user_email]'");
+		$resultado = $conexao->executaComando($query);
+
+		if(mysqli_num_rows($resultado) != 0){
+			while($linha = mysqli_fetch_array($resultado)){
+
+				$data_resposta_completa = date_create($linha["data_resposta"]);
+				$hora_resposta = date_format($data_resposta_completa, "H:i");
+				$data_resposta = date_format($data_resposta_completa, "d/m/Y");
+				$data_resposta_completa = date_format($data_resposta_completa, "Y-m-d H:i:s");
+
+				$questionarios[$contador] = array(
+					"quest_aluno" => $linha["user_nome"],
+					"quest_aluno_sobrenome" => $linha["aluno_sobrenome"],
+					"quest_nome" => $linha["quest_nome"],
+					"quest_materia" => $linha["materia_nome"],
+					"turma_nome" => $linha["turma_nome"],
+					"data_resposta" => $data_resposta." ".$hora_resposta,
+					"nota_valor" => $linha["nota_valor"],
+				);
+
+				$contador++;
+			}
+			echo json_encode($questionarios);
+		}else{
+			echo json_encode(0);
+		}
+	}
+	
 ?>
